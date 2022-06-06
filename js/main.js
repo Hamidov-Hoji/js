641562fcadecb4a53a3cb48dcfc3e203e16bbf29
Hamidov-Hoji <hojiakbarhamidov272@gmail.com>
2022-06-06 17:05:18 +0500

    git init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var firstName = prompt("Imingiz nima ?");
+var span = prompt("HTML dagi span tagi nima uchun kerak ?");
+var ozgaruvchi = prompt("let bn const nechinchi yilda qoshilgan JS ga ");
+var js = prompt("kim tomonidan ixtiro qilingan");
+var age = prompt("Yoshingizni kiriting");
+var isMarried = prompt("Uylanganmisiz?")
+
+console.log(
+  "Ismi: " + 
+  firstName +
+  '\n' + 
+  "Span: " +
+  span +
+  "Let bn Const: " +
+  ozgaruvchi +
+  "js: " +
+  js +
+  "Yosh: " +
+  age +
+  "Uylanganmisiz: " +
+  isMarried
+)
